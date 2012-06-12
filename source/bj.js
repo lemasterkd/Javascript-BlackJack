bf52741a21da05ad5117da5e23d8a490a1780863
Keith Lemaster <lemasterkd@vcu.edu>
2012-06-11 23:03:48 -0400

    Unsuccessful Jquery Display Code
    Need to figure out if selectors are working -- start out more basic and
    flesh the function out. Maybe revert.

--- source/bj.js
+++ source/bj.js
@@ -13,23 +13,24 @@
         return number;
     };
     this.getValue = function() {
-        if (number > 10){
+        if (number > 9){
             return 10;
-        } else if (number === 1){
+        } else if (number === 0){
             return 11;
         } else {
-            return number;
+            return number + 1;
         }
     };
 }
 
-function Deck (shoeSize) {
+function Deck (shoeS) {
 
 	var shoe = [];
+	var shoeSize = shoeS;
 	//filling the shoe with enough decks of correct cards
 	for (i = 0; i < shoeSize; i++) {
-		for(i = 1; i <= 4; i++) {
-			for(j = 1; j <= 13; j++) {
+		for(i = 0; i < 4; i++) {
+			for(j = 0; j < 13; j++) {
 				shoe.push(new Card(i,j));
 			}
 		}
@@ -44,7 +45,8 @@ function Deck (shoeSize) {
 	};
 }
 
-function Player (deck) {
+function Player (d) {
+	var deck = d;
 	var cards = [deck.deal(), deck.deal()];
 	
 	this.getHand = function () {
@@ -71,8 +73,9 @@ function Player (deck) {
 }
 Dealer.prototype = new Player();
 Dealer.prototype.constructor = Dealer; //something to do with constructors? investigate if things are wonky..
-function Dealer (deck) {
+function Dealer (d) {
 	//dealer's rules for auto play
+	var deck = d;
 	this.hit = function () {
 		if (this.getScore() >= 17) {
 			return false;
@@ -82,15 +85,41 @@ function Dealer (deck) {
 	};
 }
 
-var startGame = function (hands) {
+var display = function (p) {
+	var hand;
+	var $playerDiv;
+	var players = p;
+	for (i = 0; i < players.length; i++){
+		hand = players[i].getHand();
+		$playerDiv = $("#player"+i);
+		for (j = 0; j < hand.length; j++){
+			//<img src="../assets/cards.png">
+			$playerDiv.html($playerDiv.html() + "<img src='../assets/cards.png'>");
+			$($playerDiv + ' img').css("top",hand[j].getSuit() * -120);
+			$($playerDiv + ' img').css("left",hand[j].getNumber() * -80);
+			$($playerDiv + ' img').css("width",80);
+			$($playerDiv + ' img').css("height",120);
+		}
+	}
+
+};
+
+var startGame = function (h) {
 	//initializing the deck, all of the players, and the dealer
 	var master = new Deck(8);
 	var players = [];
+	var hands = h;
 	for (i = 0; i < hands; i++) {
 		players.push(new Player(master));
 	}
 	var deal = new Dealer(master);
+	display(players);
 };
+
+$('document').ready(function() {
+	startGame(4);
+
+});
 
 //for displaying the hands and such, create a display function which iterates through all of the players, drawing their hand (or upturned card?) to the board
 //this can be called at the beginning of the game, after every update refresh (ie, player presses hit or stay) and at the end of the game for the final reveal
